17d406381d3f20bd286a873bfca6ed52be80c0d3
BradlyVinceGorgonio <118922196+BradlyVinceGorgonio@users.noreply.github.com>
2023-06-02 00:11:31 +0800

    gallaron

---
+++ app/src/main/java/com/example/mainpage/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mainpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.core.widget.TextViewOnReceiveContentListener
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
